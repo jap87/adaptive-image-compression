a972f98ee1ad748ab69beea65c480c8570d1eadb
Sachal Dhillon <sachal.dhillon@gmail.com>
2019-05-01 05:35:51 -0400

    Changed css, added lossy picture

--- Mask_RCNN/PROJECT_FILES/static/js/main.js
+++ Mask_RCNN/PROJECT_FILES/static/js/main.js
@@ -7,9 +7,48 @@ function upload() {
     request.onload = function (e) {
         if (request.readyState === 4) {
           if (request.status === 200) {
-            var mask_image = document.createElement('div');
-            mask_image.innerHTML = `<img src="/static/images/mask.png" alt="Related Book 1">`;
+            
+
+
+           direct = JSON.parse(request.response)
+
+
+            var mask_image = document.createElement('div');     
+            mask_image.innerHTML = `
+            <h2>Feature Masks</h2>
+            <img src="/static/images/mask.png" alt="Related Book 1">`;
             document.body.appendChild(mask_image);
+            mask_image.setAttribute('class', 'imgDisplay')
+
+
+            var lossless_image = document.createElement('div');
+            lossless_image.innerHTML = `
+            <h2>Lossless Compression</h2>
+            <img src="/static/images/lossless.png" alt="Related Book 4">
+             <br> 
+             <p>File Size = ${(direct['lossless.png']/1024).toFixed(2)} kb</p>`;
+            document.body.appendChild(lossless_image);
+            lossless_image.setAttribute('class', 'imgDisplay')
+
+            var lossy_image = document.createElement('div');
+            lossy_image.innerHTML = `
+            <h2>Lossy Compression</h2>
+            <img src="/static/images/lossy.jpg" alt="Related Book 3">
+             <br> 
+             <p>File Size = ${(direct['lossy.jpg']/1024).toFixed(2)} kb</p>`;
+            document.body.appendChild(lossy_image);
+            lossy_image.setAttribute('class', 'imgDisplay')
+
+            var hybrid_image = document.createElement('div');
+            hybrid_image.innerHTML = `
+            <h2>Hybrid Compression</h2>
+            <img src="/static/images/hybrid.png" alt="Related Book 2">
+             <br> 
+             <p>File Size = ${(direct['hybrid.png']/1024).toFixed(2)} kb</p>`;
+            document.body.appendChild(hybrid_image);
+            hybrid_image.setAttribute('class', 'imgDisplay')
+
+
           } else {
             console.error(request.statusText);
           }
